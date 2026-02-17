dc09c20ddda90ac0d8b72bbbe2501a03caa550c4
tebolicious <mulaudzithendo113@gmail.com>
2026-02-17 10:05:36 +0000

    Fix React Router path from /funder-fleet-manager to / for FTP deployment

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
         ) : (
           <BrowserRouter>
             <Routes>
-              <Route path="/funder-fleet-manager" element={<Index />} />
+              <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
